50342e8ae0f3fd1f7dfdedc358293bd166d45c65
purerosefallen <78877@qq.com>
2019-06-26 14:18:08 +0800

    fix

--- expansions/script/c77765002.lua
+++ expansions/script/c77765002.lua
@@ -14,10 +14,12 @@ function c77765002.initial_effect(c)
 	--
 	 local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetCategory(CATEGORY_TOGRAVE)
 	e1:SetRange(LOCATION_FZONE)
 	e1:SetCountLimit(1)
 	e1:SetTarget(function(e,tp,eg,ep,ev,re,r,rp,chk)
 		if chk==0 then return Duel.GetLocationCount(1-tp,LOCATION_SZONE,tp)>0 and Duel.IsExistingMatchingCard(c77765002.filter1,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil) end
+        Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,LOCATION_DECK+LOCATION_HAND)
 	end)
 	e1:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
 		if Duel.GetLocationCount(1-tp,LOCATION_SZONE,tp)<=0 then return end
