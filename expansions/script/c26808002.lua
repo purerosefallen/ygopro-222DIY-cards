1de4f6ee999d40929c23ad3637aeeb232f1d94f8
TanakaKotoha <miho_kohinata@hotmail.com>
2019-09-28 13:00:07 +0800

    fix

--- expansions/script/c26808002.lua
+++ expansions/script/c26808002.lua
@@ -57,3 +57,13 @@ end
 function c26808002.drcon2(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
 end
+function c26808002.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c26808002.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
